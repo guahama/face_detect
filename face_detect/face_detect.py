7b155d50210f86844aa99d6294edb0c9f60a6019
guahama <chldudfkr119@kookmin.ac.kr>
2024-06-06 17:51:36 +0900

    no face error handle

--- face_detect/face_detect.py
+++ face_detect/face_detect.py
@@ -8,11 +8,14 @@ class ImageBlur():
 
     def blur(self, img_path):
         image = cv2.imread(img_path)
-        face_locations = DeepFace.extract_faces(img_path=img_path, detector_backend='mtcnn')
-        for face_location in face_locations:
-            x, y, w, h = face_location['facial_area']['x'], face_location['facial_area']['y'], face_location['facial_area']['w'], face_location['facial_area']['h'] 
-            face_image = image[y:y+h, x:x+w]
-            face_image = cv2.blur(face_image, (50, 50))
-            image[y:y+h, x:x+w] = face_image
+        try:
+            face_locations = DeepFace.extract_faces(img_path=img_path, detector_backend='mtcnn')
+            for face_location in face_locations:
+                x, y, w, h = face_location['facial_area']['x'], face_location['facial_area']['y'], face_location['facial_area']['w'], face_location['facial_area']['h'] 
+                face_image = image[y:y+h, x:x+w]
+                face_image = cv2.blur(face_image, (50, 50))
+                image[y:y+h, x:x+w] = face_image
+        except:
+            print("there's no face")
 
         return image
